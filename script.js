5216127e3c581cce2d87986da25fe52fc6cc94b2
moonahmed <mdmoonahmedkhan10@gmail.com>
2025-08-28 02:33:09 +0600

    js file added

---
+++ script.js
@@ -0,0 +1,5 @@
+const btnMenu = document.getElementById("btn-history");
+      const menu = document.getElementById("menu-history");
+      btnMenu.addEventListener("click", function () {
+        menu.classList.toggle("hidden");
+      });
